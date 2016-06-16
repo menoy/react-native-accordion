c2c43ddc027ebaac1b08722712e8146d70591c62
Aldi Reinaldi <aldi.reinaldi@expecc.com>
2016-06-16 19:05:07 +0700

    Update
    Update again

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import {
   StyleSheet,
   TouchableHighlight,
   View,
-  Text,
-  Platform,
+  Text
 } from 'react-native';
 
 var Accordion = React.createClass({
@@ -40,8 +39,8 @@ var Accordion = React.createClass({
   getInitialState() {
     return {
       is_visible: false,
-      height: 0,
-      content_height: 0
+      height: 0.0000000001,
+      content_height: 0.0000000001
     };
   },
 
@@ -59,7 +58,7 @@ var Accordion = React.createClass({
     this.tweenState('height', {
       easing: tweenState.easingTypes[this.props.easing],
       duration: this.props.animationDuration,
-      endValue: this.state.height === 0 ? this.state.content_height : 0
+      endValue: this.state.height === 0.0000000001 ? this.state.content_height : 0.0000000001
     });
   },
 
@@ -76,7 +75,7 @@ var Accordion = React.createClass({
       this.refs.AccordionContent.measure((ox, oy, width, height, px, py) => {
         // Sets content height in state
         this.setState({
-          height: this.props.expanded ? height : 0,
+          height: this.props.expanded ? height : 0.0000000001,
           content_height: height
         });
       });
@@ -111,10 +110,9 @@ var Accordion = React.createClass({
           style={{
             height: this.getTweeningValue('height')
           }}
-          onLayout={()=>{}}
         >
-          <View ref="AccordionContent" onLayout={()=>{}}>
-             {(Platform.OS === 'ios' || this.state.is_visible) ? this.props.content : null}
+          <View ref="AccordionContent">
+            {this.props.content}
           </View>
         </View>
       </View>
